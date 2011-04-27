9e7fd092bb6c136f235c99dcd3b1564f09f4030e
Akihiro Matsumura <matsumura.aki@gmail.com>
2011-04-27 20:07:35 +0900

    Change heroku.js format

--- Resources/heroku.js
+++ Resources/heroku.js
@@ -63,95 +63,6 @@ var client = function(method, path, options){
   }
 };
 
-exports.login = function(email, password, callback){
-  client('POST', "/login", {
-    onloadCallback: function(){
-      var result = JSON.parse(this.responseText);
-      callback.call(this, true, {email: result.email, apiKey: result.api_key});
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    postBody: {username: email, password: password}
-  });
-};
-
-exports.list = function(login, callback){
-  client('GET', "/apps", {
-    onloadCallback: function(){
-      var result = JSON.parse(this.responseText);
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
-
-
-exports.restart = function(login, appName, callback){
-  var path = '/apps/' + appName + '/server';
-  client('DELETE', path, {
-    onloadCallback: function(){
-      callback.call(this, true);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
-
-exports.maintenance = function(login, appName, status, callback){
-  var path = "/apps/" + appName + "/server/maintenance";
-  var mode = status == true ? '1' : '0';
-  client('POST', path, {
-    onloadCallback: function(){
-      callback.call(this, true);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login,
-    postBody: {maintenance_mode: mode}
-  });
-};
-
-exports.info = function(login, appName, callback){
-  var path = "/apps/" + appName;
-  client('GET', path, {
-    onloadCallback: function(){
-      var doc = this.responseXML.documentElement,
-      dynos = doc.getElementsByTagName('dynos').item(0).text,
-      workers = doc.getElementsByTagName('workers').item(0).text,
-      stack = doc.getElementsByTagName('stack').item(0).text,
-      repo = doc.getElementsByTagName('repo-size').item(0).text,
-      slug = doc.getElementsByTagName('slug-size').item(0).text,
-      data = doc.getElementsByTagName('database_size').item(0).text,
-      appUrl = doc.getElementsByTagName('web_url').item(0).text,
-      gitUrl = doc.getElementsByTagName('git_url').item(0).text;
-
-      callback.call(this, {
-        dynos: dynos,
-        workers: workers,
-        stack: stack,
-        repo: repo,
-        slug: slug,
-        data: data,
-        appUrl: appUrl,
-        gitUrl: gitUrl
-      });
-    },
-    onerrorCallback: function(){
-      Ti.API.debug(this.responseText);
-      callback.call(this, null);
-    },
-    login: login,
-    contentType: "application/xml"
-  });
-};
-
 var updateDynosOrWorkers = function(login, path, qty, callback){
   client("PUT", path, {
     onloadCallback: function(){
@@ -168,111 +79,200 @@ var updateDynosOrWorkers = function(login, path, qty, callback){
   });
 };
 
-exports.adjustDynos = function(login, appName, qty, callback){
-  var path = "/apps/" + appName + "/dynos";
-  updateDynosOrWorkers(login, path, qty, callback);
-};
+exports = {
+  login: function(email, password, callback){
+    client('POST', "/login", {
+      onloadCallback: function(){
+        var result = JSON.parse(this.responseText);
+        callback.call(this, true, {email: result.email, apiKey: result.api_key});
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      postBody: {username: email, password: password}
+    });
+  },
+  list: function(login, callback){
+    client('GET', "/apps", {
+      onloadCallback: function(){
+        var result = JSON.parse(this.responseText);
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
 
-exports.adjustWorkers = function(login, appName, qty, callback){
-  var path = "/apps/" + appName + "/workers";
-  updateDynosOrWorkers(login, path, qty, callback);
-};
+  restart: function(login, appName, callback){
+    var path = '/apps/' + appName + '/server';
+    client('DELETE', path, {
+      onloadCallback: function(){
+        callback.call(this, true);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
 
-exports.addons = function(login, appName, callback){
-  var path = "/apps/" + appName + "/addons";
-  client('GET', path, {
-    onloadCallback: function(){
-      var result = JSON.parse(this.responseText);
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
+  maintenance: function(login, appName, status, callback){
+    var path = "/apps/" + appName + "/server/maintenance";
+    var mode = status == true ? '1' : '0';
+    client('POST', path, {
+      onloadCallback: function(){
+        callback.call(this, true);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login,
+      postBody: {maintenance_mode: mode}
+    });
+  },
 
-exports.collaborators = function(login, appName, callback){
-  var path = "/apps/" + appName + "/collaborators";
-  client('GET', path, {
-    onloadCallback: function(){
-      var doc = this.responseXML.documentElement,
-      email_nodes = doc.getElementsByTagName('email'),
-      emails = [];
+  info: function(login, appName, callback){
+    var path = "/apps/" + appName;
+    client('GET', path, {
+      onloadCallback: function(){
+        var doc = this.responseXML.documentElement,
+        dynos = doc.getElementsByTagName('dynos').item(0).text,
+        workers = doc.getElementsByTagName('workers').item(0).text,
+        stack = doc.getElementsByTagName('stack').item(0).text,
+        repo = doc.getElementsByTagName('repo-size').item(0).text,
+        slug = doc.getElementsByTagName('slug-size').item(0).text,
+        data = doc.getElementsByTagName('database_size').item(0).text,
+        appUrl = doc.getElementsByTagName('web_url').item(0).text,
+        gitUrl = doc.getElementsByTagName('git_url').item(0).text;
 
-      for(var i=0; i<email_nodes.length; i++){
-        emails.push(email_nodes.item(i).text);
-      }
+        callback.call(this, {
+          dynos: dynos,
+          workers: workers,
+          stack: stack,
+          repo: repo,
+          slug: slug,
+          data: data,
+          appUrl: appUrl,
+          gitUrl: gitUrl
+        });
+      },
+      onerrorCallback: function(){
+        Ti.API.debug(this.responseText);
+        callback.call(this, null);
+      },
+      login: login,
+      contentType: "application/xml"
+    });
+  },
 
-      callback.call(this, emails);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login,
-    contentType: "application/xml"
-  });
-};
+  adjustDynos: function(login, appName, qty, callback){
+    var path = "/apps/" + appName + "/dynos";
+    updateDynosOrWorkers(login, path, qty, callback);
+  },
 
-exports.logs = function(login, appName, callback){
-  var path = "/apps/" + appName + "/logs";
-  client('GET', path, {
-    onloadCallback: function(){
-      var result = this.responseText;
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
+  adjustWorkers: function(login, appName, qty, callback){
+    var path = "/apps/" + appName + "/workers";
+    updateDynosOrWorkers(login, path, qty, callback);
+  },
 
-exports.cronLogs = function(login, appName, callback){
-  var path = "/apps/" + appName + "/cron_logs";
-  client('GET', path, {
-    onloadCallback: function(){
-      var result = this.responseText;
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
+  addons: function(login, appName, callback){
+    var path = "/apps/" + appName + "/addons";
+    client('GET', path, {
+      onloadCallback: function(){
+        var result = JSON.parse(this.responseText);
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
 
-exports.configVars = function(login, appName, callback){
-  var path = "/apps/" + appName + "/config_vars";
-  client('GET', path, {
-    onloadCallback: function(){
-      var result = JSON.parse(this.responseText);
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      callback.call(this, false);
-    },
-    login: login
-  });
-};
+  collaborators: function(login, appName, callback){
+    var path = "/apps/" + appName + "/collaborators";
+    client('GET', path, {
+      onloadCallback: function(){
+        var doc = this.responseXML.documentElement,
+        email_nodes = doc.getElementsByTagName('email'),
+        emails = [];
 
-// TODO: Need to request with chunks, ref heroku.gem
-exports.rake = function(login, appName, cmd, callback){
-  var path = "/apps/" + appName + "/services";
-  var body = "rake " + cmd;
-  client('POST', path, {
-    onloadCallback: function(){
-      Ti.API.debug(this.responseText);
-      var result = this.responseText;
-      callback.call(this, result);
-    },
-    onerrorCallback: function(){
-      Ti.API.debug(this.responseText);
-      var result = this.responseText;
-      callback.call(this, result);
-    },
-    login: login,
-    postBody: body,
-    isStrBody: true
-  });
+        for(var i=0; i<email_nodes.length; i++){
+          emails.push(email_nodes.item(i).text);
+        }
+
+        callback.call(this, emails);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login,
+      contentType: "application/xml"
+    });
+  },
+
+  logs: function(login, appName, callback){
+    var path = "/apps/" + appName + "/logs";
+    client('GET', path, {
+      onloadCallback: function(){
+        var result = this.responseText;
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
+
+  cronLogs: function(login, appName, callback){
+    var path = "/apps/" + appName + "/cron_logs";
+    client('GET', path, {
+      onloadCallback: function(){
+        var result = this.responseText;
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
+
+  configVars: function(login, appName, callback){
+    var path = "/apps/" + appName + "/config_vars";
+    client('GET', path, {
+      onloadCallback: function(){
+        var result = JSON.parse(this.responseText);
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        callback.call(this, false);
+      },
+      login: login
+    });
+  },
+
+  // TODO: Need to request with chunks, ref heroku.gem
+  rake: function(login, appName, cmd, callback){
+    var path = "/apps/" + appName + "/services";
+    var body = "rake " + cmd;
+    client('POST', path, {
+      onloadCallback: function(){
+        Ti.API.debug(this.responseText);
+        var result = this.responseText;
+        callback.call(this, result);
+      },
+      onerrorCallback: function(){
+        Ti.API.debug(this.responseText);
+        var result = this.responseText;
+        callback.call(this, result);
+      },
+      login: login,
+      postBody: body,
+      isStrBody: true
+    });
+  }
 };
